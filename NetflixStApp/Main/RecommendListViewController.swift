dd6c4dfcd7298350e5f323cb2dd95c70fb60fa6c
Sean Cho <74293674+chxhyxn@users.noreply.github.com>
2022-05-27 05:46:49 +0900

    init app

---
+++ NetflixStApp/Main/RecommendListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecommendListViewController.swift
+//  NetflixStApp
+//
+//  Created by chxhyxn on 2022/05/27.
+//
+
+import UIKit
+
+class RecommendListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NetflixStApp/Player/PlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerView.swift
+//  NetflixStApp
+//
+//  Created by chxhyxn on 2022/05/27.
+//
+
+import UIKit
+
+class PlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
