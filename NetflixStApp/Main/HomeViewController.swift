d77a18db4d97607eb5cc3604bfcec04f33bfbbd1
Sean Cho <74293674+chxhyxn@users.noreply.github.com>
2022-06-16 11:50:40 +0900

    update homeview

--- NetflixStApp/Main/HomeViewController.swift
+++ NetflixStApp/Main/HomeViewController.swift
@@ -6,24 +6,36 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class HomeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    var awardRecommendListViewController: RecommendListViewController!
+    var hotRecommendListViewController: RecommendListViewController!
+    var myRecommendListViewController: RecommendListViewController!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "award" {
+            let destinationVC = segue.destination as? RecommendListViewController
+            awardRecommendListViewController = destinationVC
+            awardRecommendListViewController.viewModel.updateType(.award)
+            awardRecommendListViewController.viewModel.fetchItems()
+        } else if segue.identifier == "hot" {
+            let destinationVC = segue.destination as? RecommendListViewController
+            hotRecommendListViewController = destinationVC
+            hotRecommendListViewController.viewModel.updateType(.hot)
+            hotRecommendListViewController.viewModel.fetchItems()
+        } else if segue.identifier == "my" {
+            let destinationVC = segue.destination as? RecommendListViewController
+            myRecommendListViewController = destinationVC
+            myRecommendListViewController.viewModel.updateType(.my)
+            myRecommendListViewController.viewModel.fetchItems()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
-    */
-
 }
--- NetflixStApp/Main/RecommendListViewController.swift
+++ NetflixStApp/Main/RecommendListViewController.swift
@@ -5,25 +5,110 @@
 //  Created by chxhyxn on 2022/05/27.
 //
 
+
 import UIKit
 
 class RecommendListViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    let viewModel = RecommendListViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateUI()
     }
-    */
 
+    func updateUI() {
+        lblTitle.text = viewModel.type.title
+    }
+}
+
+extension RecommendListViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.numOfItems
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RecommendCell", for: indexPath) as? RecommendCell else {
+            return UICollectionViewCell()
+        }
+        
+        let movie = viewModel.item(at: indexPath.item)
+        cell.updateUI(movie: movie)
+        return cell
+    }
+}
+
+extension RecommendListViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 120, height: 160)
+    }
+}
+
+class RecommendListViewModel {
+    enum RecommendingType {
+        case award
+        case hot
+        case my
+        
+        var title: String {
+            switch self {
+            case .award: return "아카데미 호평 영황"
+            case .hot: return "취한저격 HOT 콘텐츠"
+            case .my: return "내가 찜한 콘텐츠"
+            
+            }
+        }
+    }
+    
+    private (set) var type: RecommendingType = .my
+    private var items: [DummyItem] = []
+    
+    var numOfItems: Int {
+        return items.count
+    }
+    
+    func item(at index: Int) -> DummyItem {
+        return items[index]
+    }
+    
+    func updateType(_ type: RecommendingType) {
+        self.type = type
+    }
+    
+    func fetchItems() {
+        self.items = MovieFetcher.fetch(type)
+    }
+}
+
+class RecommendCell: UICollectionViewCell {
+    @IBOutlet weak var imgThumbnail: UIImageView!
+    
+    func updateUI(movie: DummyItem) {
+        imgThumbnail.image = movie.thumbnail
+    }
+}
+
+class MovieFetcher {
+    static func fetch(_ type: RecommendListViewModel.RecommendingType) -> [DummyItem] {
+        switch type {
+        case .award:
+            let movies = (1..<10).map { DummyItem(thumbnail: UIImage(named: "imgMovie\($0)")!) }
+            return movies
+        case .hot:
+            let movies = (10..<19).map { DummyItem(thumbnail: UIImage(named: "imgMovie\($0)")!) }
+            return movies
+        case .my:
+            let movies = (1..<10).map { $0 * 2 }.map { DummyItem(thumbnail: UIImage(named: "imgMovie\($0)")!) }
+            return movies
+        }
+    }
+}
+
+struct DummyItem {
+    let thumbnail: UIImage
 }
